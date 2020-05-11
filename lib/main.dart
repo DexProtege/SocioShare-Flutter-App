65e9e1aa88a64cd7b92073a3d13cd81830440dde
DexProtege <dwipmakwana@gmail.com>
2020-05-11 21:26:47 +0530

    done

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'pages/home.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firestore.instance.settings(timestampsInSnapshotsEnabled: true).then((_){
+    //print("Timestamp enabled in snapshot\n");
+    }, onError: (_) {
+      //print("Error enabling timestamp in snapshot\n");
+    }
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SocioShare',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+        accentColor: Colors.purple
+      ),
+      home: Home(),
+    );
+  }
+}
